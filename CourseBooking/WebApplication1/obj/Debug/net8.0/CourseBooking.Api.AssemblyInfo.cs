5e3349ca5b4cc4c040e59249a2326ab08e705fa6
Abdelrhaman Khater (ESTARTA SOLUTIONS) <v-akhater@microsoft.com>
2026-02-05 13:59:12 +0200

    Clean repo and ignore VS files

--- CourseBooking/WebApplication1/obj/Debug/net8.0/CourseBooking.Api.AssemblyInfo.cs
+++ CourseBooking/WebApplication1/obj/Debug/net8.0/CourseBooking.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CourseBooking.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd06a11e441d1053f8c3cc2a0aedb1ba44d9b109")]
 [assembly: System.Reflection.AssemblyProductAttribute("CourseBooking.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CourseBooking.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
